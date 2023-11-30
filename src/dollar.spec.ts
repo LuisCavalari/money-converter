2081134b41344e8050a33346e3872cce58fbd861
Luis Cavalari <luiscavalari@ufpr.br>
2023-11-29 22:07:05 -0300

    test: add dollar describe section

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -1,7 +1,8 @@
 import { Dollar } from './dollar'
-
-it('should return 10 when five dollars are multiplied by 2', () => {
-  const five = new Dollar(5)
-  five.times(2)
-  expect(five.amount).toBe(10)
+describe('Dollar', () => {
+  it('should return 10 when five dollars are multiplied by 2', () => {
+    const five = new Dollar(5)
+    five.times(2)
+    expect(five.amount).toBe(10)
+  })
 })
