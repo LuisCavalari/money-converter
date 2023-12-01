2e12f99a7762a4eb4a982d41b55f731c26b2e7f7
Luis Cavalari <luiscavalari@ufpr.br>
2023-11-30 21:31:33 -0300

    test: remove dollar side-effect

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -1,8 +1,13 @@
 import { Dollar } from './dollar'
+
 describe('Dollar', () => {
   it('should return 10 when five dollars are multiplied by 2', () => {
-    const five = new Dollar(5)
-    five.times(2)
-    expect(five.amount).toBe(10)
+    const five: Dollar = new Dollar(5)
+    let product: Dollar = five.times(2)
+
+    expect(product.amount).toEqual(10)
+
+    product = five.times(3)
+    expect(product.amount).toEqual(15)
   })
 })
