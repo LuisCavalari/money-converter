36c17d37639092487ae8cc1a898d11f96ff18354
Luis Cavalari <luiscavalari@ufpr.br>
2023-11-30 21:48:25 -0300

    test: dollar comparison

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -10,4 +10,12 @@ describe('Dollar', () => {
     product = five.times(3)
     expect(product.amount).toEqual(15)
   })
+
+  it('should return true if dollars has the same value', () => {
+    expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy()
+  })
+
+  it('should return false dollar has differents values', () => {
+    expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy()
+  })
 })
