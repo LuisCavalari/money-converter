3a6bd97702fc47d30e85188ee6c6e654d1cbe8a9
Luis Cavalari <luiscavalari@ufpr.br>
2023-12-04 21:57:49 -0300

    test: make equals in money upper class

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -1,6 +1,7 @@
 import { Dollar } from './dollar'
+import { Euro } from './euro'
 
-describe('Dollar', () => {
+describe('Money', () => {
   it('should return 10 when five dollars are multiplied by 2 and 15 when is multiplied by 3', () => {
     const five: Dollar = new Dollar(5)
 
@@ -15,5 +16,21 @@ describe('Dollar', () => {
 
   it('should return false dollar has differents values', () => {
     expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy()
+  })
+
+  it('should return 10 when five Euros are multiplied by 2 and 15 when is multiplied by 3', () => {
+    const five: Euro = new Euro(5)
+
+    expect(five.times(2).equals(new Euro(10))).toBeTruthy()
+
+    expect(five.times(3).equals(new Euro(15))).toBeTruthy()
+  })
+
+  it('should return true if Euros has the same value', () => {
+    expect(new Euro(5).equals(new Euro(5))).toBeTruthy()
+  })
+
+  it('should return false Euro has differents values', () => {
+    expect(new Euro(5).equals(new Euro(6))).toBeFalsy()
   })
 })
